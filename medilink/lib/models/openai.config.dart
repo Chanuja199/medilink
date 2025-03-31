a9aec5f9a8e5dde29f2c5c89a48c28f2b444d7da
dspchanuja@students.nsbm.ac.lk <dspchanuja@students.nsbm.ac.lk>
2025-03-31 22:44:16 +0530

    removed the key

--- medilink/lib/models/openai.config.dart
+++ medilink/lib/models/openai.config.dart
@@ -1,5 +1,5 @@
 class OpenAIConfig {
-  static const String apiKey = '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';
+  static const String apiKey = '';
   static const String baseUrl = 'https://api.openai.com/v1';
   static const String gpt4VisionModel = 'gpt-4o';
   static const String gpt3Model = 'gpt-3.5-turbo';
